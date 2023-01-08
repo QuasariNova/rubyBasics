438352c33881a76fa9721876e5afed5f88d7e222
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-08 10:57:13 -0500

    Strings: Exercise7 complete

---
+++ strings/exercise7.rb
@@ -0,0 +1,14 @@
+# Given the following code, invoke a destructive method on greeting so that 
+# Goodbye! is printed instead of Hello!.
+# 
+# greeting = 'Hello!'
+# puts greeting
+# 
+# Expected output:
+# 
+# Goodbye!
+
+greeting = 'Hello!'
+
+greeting.replace 'Goodbye!'
+puts greeting
