c9483c278a1095ce72274e3f9eb03dd817ff4396
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-08 11:06:45 -0500

    Strings: Exercise 10 complete

---
+++ strings/exercise10.rb
@@ -0,0 +1,15 @@
+# Using the following code, print true if colors includes the color 'yellow' 
+# and print false if it doesn't. Then, print true if colors includes the color 
+# 'purple' and print false if it doesn't.
+# 
+# colors = 'blue pink yellow orange'
+# 
+# Expected output:
+# 
+# true
+# false
+
+colors = 'blue pink yellow orange'
+
+puts colors.include?('yellow')
+puts colors.include?('purple')
