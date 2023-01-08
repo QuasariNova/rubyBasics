17f2345f0eaecd02ae374f9aa366d8b10900276d
Christopher Perkins <Christopher.Perkins@null.net>
2023-01-08 10:08:40 -0500

    Conditionals: Exercise 1 complete

---
+++ conditionals/exercise1.rb
@@ -0,0 +1,14 @@
+#In the code below, sun is randomly assigned as 'visible' or 'hidden'.
+# 
+# sun = ['visible', 'hidden'].sample
+# 
+# Write an if statement that prints "The sun is so bright!" if sun equals 
+#'visible'.
+
+sun = ['visible', 'hidden'].sample
+
+if sun == 'visible'
+  puts "The sun is so bright!"
+end
+
+# I'd rather put the if at the end, but feel like this will be built on.
